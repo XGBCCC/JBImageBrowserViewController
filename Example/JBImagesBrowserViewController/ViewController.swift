70aec03c29450ff0d696f355e83a3c65350d6f9c
JimBo <eric_jingbo@126.com>
2016-02-22 13:49:19 +0800

    1.change the directory structure 2.add Pods files

--- Example/JBImagesBrowserViewController/ViewController.swift
+++ Example/JBImagesBrowserViewController/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         let imageURL_3 = NSBundle.mainBundle().URLForResource("IMG_5429", withExtension: "JPG")
         
         
-        let imageBrowserVC = JBImageBrowserViewController(frame:CGRect(x: 0, y: 0, width: CGRectGetWidth(self.view.bounds), height: CGRectGetHeight(self.view.bounds)),imageArray: [JBImage(_url: NSURL(string: "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c0.134.1080.1080/12717038_1023352297739927_791033668_n.jpg?ig_cache_key=MTE4NzYzMzM5NDgyMTc5NTM5OQ%3D%3D.2.c")),JBImage(_url: NSURL(string: "https://scontent.cdninstagram.com/t51.2885-15/e15/12750120_1058587360850246_2078112053_n.jpg?ig_cache_key=MTE4NzgwNTk4MjUyNTUxNzM5Ng%3D%3D.2")),JBImage(_filePathURL: imageURL_1),JBImage(_filePathURL: imageURL_2),JBImage(_filePathURL: imageURL_3),JBImage(_url: NSURL(string: "https://raw.githubusercontent.com/onevcat/Kingfisher/master/images/logo.png"))])
+        let imageBrowserVC = JBImageBrowserViewController(frame:CGRect(x: 0, y: 0, width: CGRectGetWidth(self.view.bounds), height: CGRectGetHeight(self.view.bounds)),imageArray: [JBImage(_url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/style_large_2x.jpg")),JBImage(_url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/hero_tvad_large_2x.jpg")),JBImage(_filePathURL: imageURL_1),JBImage(_filePathURL: imageURL_2),JBImage(_filePathURL: imageURL_3),JBImage(_url: NSURL(string: "https://raw.githubusercontent.com/onevcat/Kingfisher/master/images/logo.png"))])
         self.presentViewController(imageBrowserVC, animated: true, completion: nil)
     }
 
---
+++ Example/Pods/Target Support Files/Pods-JBImageBrowserViewController/Pods-JBImageBrowserViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JBImageBrowserViewController : NSObject
+@end
+@implementation PodsDummy_Pods_JBImageBrowserViewController
+@end
