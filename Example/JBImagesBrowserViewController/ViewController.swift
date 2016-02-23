2a39e2a5280f9ab059f2dfc69350cc464677f219
JimBo <eric_jingbo@126.com>
2016-02-23 17:06:30 +0800

    add readme.md

--- Example/JBImagesBrowserViewController/ViewController.swift
+++ Example/JBImagesBrowserViewController/ViewController.swift
@@ -17,13 +17,15 @@ class ViewController: UIViewController {
     
     @IBAction func buttonDidTouched(sender: UIButton) {
         
-        let imageURL_1 = NSBundle.mainBundle().URLForResource("IMG_5445", withExtension: "JPG")
-        let imageURL_2 = NSBundle.mainBundle().URLForResource("IMG_5436", withExtension: "PNG")
-        let imageURL_3 = NSBundle.mainBundle().URLForResource("IMG_5429", withExtension: "JPG")
         
+        let image_1 = JBImage(url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/style_large_2x.jpg"))
+        let image_2 = JBImage(filePathURL: NSBundle.mainBundle().URLForResource("IMG_5445", withExtension: "JPG"))
+        let image_3 = JBImage(image: UIImage(named: "IMG_5429"))
         let failedImage = UIImage(named: "error_place_image")
         
-        let imageBrowserVC = JBImageBrowserViewController(frame:CGRect(x: 0, y: 0, width: CGRectGetWidth(self.view.bounds), height: CGRectGetHeight(self.view.bounds)),imageArray: [JBImage(url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/style_large_2x.jpg")),JBImage(url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/hero_tvad_large_2x.jpg")),JBImage(filePathURL: imageURL_1),JBImage(filePathURL: imageURL_2),JBImage(filePathURL: imageURL_3),JBImage(url: NSURL(string: "https://raw.githubusercontent.com/onevcat/Kingfisher/master/images/logo.png")),JBImage(url:NSURL(string: "https://www.baidu.com/error.png"))],failedPlaceholderImage:failedImage)
+        
+        let imageBrowserVC = JBImageBrowserViewController(frame:CGRect(x: 0, y: 0, width: CGRectGetWidth(self.view.bounds), height: CGRectGetHeight(self.view.bounds)),imageArray: [image_1,image_2,image_3],failedPlaceholderImage:failedImage)
+        
         self.presentViewController(imageBrowserVC, animated: true, completion: nil)
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JBImageBrowserViewController
+JBImageBrowserViewController is a library enables a user to present images in fullscreen like twitter. Besides the typical pinch and double tap to zoom, we also provide a vertical swipe to dismiss. 
+
+![](Documentation/preview.gif)
+
+##Usage
+
+create some images 
+
+```swift
+let image_1 = JBImage(url: NSURL(string: "http://images.apple.com/cn/chinese-new-year/images/style_large_2x.jpg"))
+let image_2 = JBImage(filePathURL: NSBundle.mainBundle().URLForResource("IMG_5445", withExtension: "JPG"))
+let image_3 = JBImage(image: UIImage(named: "IMG_5429"))
+let failedImage = UIImage(named: "error_place_image")
+```
+<br/>create an instance view controller for parsent images
+
+```swift
+let imageBrowserVC = JBImageBrowserViewController(frame:CGRect(x: 0, y: 0, width: CGRectGetWidth(self.view.bounds), height: CGRectGetHeight(self.view.bounds)),imageArray: [image_1,image_2,image_3],failedPlaceholderImage:failedImage)
+self.presentViewController(imageBrowserVC, animated: true, completion: nil)
+```
