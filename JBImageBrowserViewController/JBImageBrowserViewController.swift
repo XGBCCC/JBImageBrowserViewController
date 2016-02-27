c8e21fab28fe8fd38ca365610ab9d274afaf0e5c
JimBo <eric_jingbo@126.com>
2016-02-28 01:14:49 +0800

    no message

--- JBImageBrowserViewController/JBImageBrowserViewController.swift
+++ JBImageBrowserViewController/JBImageBrowserViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Kingfisher
 
+
 public class JBImageBrowserViewController: UIViewController {
 
     private var imageList:[JBImage]!
@@ -82,8 +83,9 @@ public class JBImageBrowserViewController: UIViewController {
         modalPresentationStyle = .OverFullScreen
         modalPresentationCapturesStatusBarAppearance = true
         
+        let activeImage = (UIImage(named: "JBImagesBrowserVC.bundle/imagesbrowser_item") != nil) ? UIImage(named: "JBImagesBrowserVC.bundle/imagesbrowser_item") : UIImage(named:"Frameworks/MJRefresh.framework/MJRefresh.bundle/imagesbrowser_item");
         activeButton = UIButton(frame: CGRect(origin: CGPoint(x: view.bounds.size.width - CGFloat(activeButtonPadding) - activeButtonSize.width, y: view.bounds.size.height - CGFloat(activeButtonPadding) - activeButtonSize.height), size: activeButtonSize))
-        activeButton?.setImage(UIImage(named: "JBImagesBrowserVC.bundle/imagesbrowser_item"), forState: .Normal)
+        activeButton?.setImage(activeImage, forState: .Normal)
         activeButton?.addTarget(self, action: Selector("activeButtonClicked"), forControlEvents: .TouchUpInside)
         
         view.addSubview(activeButton!)
